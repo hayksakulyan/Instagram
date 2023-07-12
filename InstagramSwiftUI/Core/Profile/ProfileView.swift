5d8be8a319dcdf6f6583488cefd89c6a9c5742a7
hayksakulyan <sakulyanhayk@gmail.com>
2023-07-12 11:40:08 +0400

    "Added buttons Share Profile and Add on Profile View"

--- InstagramSwiftUI/Core/Profile/ProfileView.swift
+++ InstagramSwiftUI/Core/Profile/ProfileView.swift
@@ -53,18 +53,45 @@ struct ProfileView: View {
                     
                     // action button
                     
-                    Button {
-                        //
-                    } label: {
-                        Text("Edit Profile")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .frame(width: 360, height: 32)
-                            .foregroundColor(.black)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 6)
-                                    .stroke(Color.gray, lineWidth: 1)
-                            }
+                    HStack {
+                        Button {
+                            //
+                        } label: {
+                            Text("Edit Profile")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .frame(width: 155, height: 32)
+                                .foregroundColor(.black)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 6)
+                                        .stroke(Color.gray, lineWidth: 1)
+                                }
+                    }
+                        Button {
+                            //
+                        } label: {
+                            Text("Share Profile")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .frame(width: 155, height: 32)
+                                .foregroundColor(.black)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 6)
+                                        .stroke(Color.gray, lineWidth: 1)
+                                }
+                    }
+                        Button {
+                            //
+                        } label: {
+                            Image(systemName: "person.badge.plus")
+                                .foregroundColor(.black)
+                                .frame(width: 32, height: 32)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 6)
+                                        .stroke(Color.gray, lineWidth: 1)
+                                }
+                        }
+
                     }
 
                     
